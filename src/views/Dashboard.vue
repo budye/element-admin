4381382f7b8bd41e6472fcec44390240dda1df8e
YuPeng <95722892@qq.com>
2017-10-31 17:39:46 +0800

    add ECharts

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,8 +1,135 @@
 <template>
-    <h1>Welcome!</h1>
+  <div>
+    <el-row class="breadcrumb">
+      <el-col :span="4"><h3>Dashboard</h3></el-col>
+      <el-col :span="20">
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item>首页</el-breadcrumb-item>
+          <el-breadcrumb-item>Dashboard</el-breadcrumb-item>
+        </el-breadcrumb>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-card>
+          <div style="height:450px;">
+            <span>haha</span>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="18">
+        <el-card>
+          <div id="main" style="width:100%;height:450px;">
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <br>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <el-card>
+          <el-table :data="tableData" style="width: 100%">
+            <el-table-column prop="date" label="日期" width="180"></el-table-column>
+            <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+            <el-table-column prop="address" label="地址"></el-table-column>
+          </el-table>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <script>
-    export default {
-        name: "Dashboard"
+  import echarts from 'echarts'
+
+  export default {
+    name: "Dashboard",
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
+    },
+    mounted: function () {
+      var myChart = echarts.init(document.getElementById('main'));
+      var option = {
+        title: {
+          text: '渠道分析'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+            data: [120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+            data: [220, 182, 191, 234, 290, 330, 310]
+          },
+          {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+            data: [150, 232, 201, 154, 190, 330, 410]
+          },
+          {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+            data: [320, 332, 301, 334, 390, 330, 320]
+          },
+          {
+            name: '搜索引擎',
+            type: 'line',
+            stack: '总量',
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+          }
+        ]
+      };
+
+      myChart.setOption(option);
     }
+  }
 </script>
